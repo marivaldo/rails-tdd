a15c85135368f1777d2be66b266081c9f90497c7
Marivaldo Malaquias <marivaldo@gmail.com>
2020-02-07 13:18:42 -0400

    test: more tests example

--- test_app/app/controllers/customers_controller.rb
+++ test_app/app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_member!, except: [:index]
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   # GET /customers
@@ -53,6 +54,6 @@ class CustomersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def customer_params
-      params.require(:customer).permit(:name, :email)
+      params.require(:customer).permit(:name, :email, :address)
     end
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -34,6 +34,18 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
+  # Devise
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+
+  # Shoulda Matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # Time Helper
   config.include ActiveSupport::Testing::TimeHelpers
 
